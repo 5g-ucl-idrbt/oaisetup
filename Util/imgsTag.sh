a4aec7ec2336a7ae1381c2e8d6252727da0ba29c
subhrendu1987 <subhrendu@idrbt.ac.in>
2022-08-21 09:06:16 +0530

    new file:   Ref/core_cmd_history.txt 	new file:   Ref/corediff.txt 	new file:   Ref/gnb_cmd_history.txt 	new file:   Ref/gnb_diff.txt 	new file:   Util/imgsTag.sh 	new file:   Util/pullimgs.sh

---
+++ Util/imgsTag.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+docker image tag oaisoftwarealliance/oai-amf:v1.4.0 oai-amf:v1.4.0
+docker image tag oaisoftwarealliance/oai-nrf:v1.4.0 oai-nrf:v1.4.0
+docker image tag oaisoftwarealliance/oai-smf:v1.4.0 oai-smf:v1.4.0
+docker image tag oaisoftwarealliance/oai-spgwu-tiny:v1.4.0 oai-spgwu-tiny:v1.4.0
+docker image tag oaisoftwarealliance/oai-udr:v1.4.0 oai-udr:v1.4.0
+docker image tag oaisoftwarealliance/oai-udm:v1.4.0 oai-udm:v1.4.0
+docker image tag oaisoftwarealliance/oai-ausf:v1.4.0 oai-ausf:v1.4.0
+docker image tag oaisoftwarealliance/oai-upf-vpp:v1.4.0 oai-upf-vpp:v1.4.0
+docker image tag oaisoftwarealliance/oai-nssf:v1.4.0 oai-nssf:v1.4.0
+docker image tag oaisoftwarealliance/trf-gen-cn5g:latest trf-gen-cn5g:latest
+
---
+++ Util/pullimgs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker pull oaisoftwarealliance/oai-amf:v1.4.0
+docker pull oaisoftwarealliance/oai-nrf:v1.4.0
+docker pull oaisoftwarealliance/oai-spgwu-tiny:v1.4.0
+docker pull oaisoftwarealliance/oai-smf:v1.4.0
+docker pull oaisoftwarealliance/oai-udr:v1.4.0
+docker pull oaisoftwarealliance/oai-udm:v1.4.0
+docker pull oaisoftwarealliance/oai-ausf:v1.4.0
+docker pull oaisoftwarealliance/oai-upf-vpp:v1.4.0
+docker pull oaisoftwarealliance/oai-nssf:v1.4.0
+# Utility image to generate traffic
+docker pull oaisoftwarealliance/trf-gen-cn5g:latest
+
