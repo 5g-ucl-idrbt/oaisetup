ac6097585ee054b50dbaafe374891128f440eb39
subhrendu1987 <subhrendu@idrbt.ac.in>
2022-09-02 10:41:49 +0530

    new file:   CORE/oai-cn5g-fed/component/mysql-5.7/docker-compose.yaml 	new file:   CORE/oai-cn5g-fed/component/mysql-5.7/mysql-healthcheck1.sh 	new file:   CORE/oai-cn5g-fed/component/mysql-5.7/mysql-healthcheck2.sh 	modified:   Util/pullimgs.sh

--- Util/pullimgs.sh
+++ Util/pullimgs.sh
@@ -2,27 +2,27 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-docker pull mysql:5.7
+sudo docker pull mysql:5.7
 echo -e "${RED} Downloaded mysql:5.7 ${NC}"
-docker pull oaisoftwarealliance/oai-amf:v1.4.0
+sudo docker pull oaisoftwarealliance/oai-amf:v1.4.0
 echo -e "${RED} Downloaded oai-amf:v1.4.0 ${NC}"
-docker pull oaisoftwarealliance/oai-nrf:v1.4.0
+sudo docker pull oaisoftwarealliance/oai-nrf:v1.4.0
 echo -e "${RED} Downloaded oai-nrf:v1.4.0 ${NC}"
-docker pull oaisoftwarealliance/oai-spgwu-tiny:v1.4.0
+sudo docker pull oaisoftwarealliance/oai-spgwu-tiny:v1.4.0
 echo -e "${RED} Downloaded oai-spgwu-tiny:v1.4.0 ${NC}"
-docker pull oaisoftwarealliance/oai-smf:v1.4.0
+sudo docker pull oaisoftwarealliance/oai-smf:v1.4.0
 echo -e "${RED} Downloaded oai-smf:v1.4.0 ${NC}"
-docker pull oaisoftwarealliance/oai-udr:v1.4.0
+sudo docker pull oaisoftwarealliance/oai-udr:v1.4.0
 echo -e "${RED} Downloaded oai-udr:v1.4.0 ${NC}"
-docker pull oaisoftwarealliance/oai-udm:v1.4.0
+sudo docker pull oaisoftwarealliance/oai-udm:v1.4.0
 echo -e "${RED} Downloaded oai-udm:v1.4.0 ${NC}"
-docker pull oaisoftwarealliance/oai-ausf:v1.4.0
+sudo docker pull oaisoftwarealliance/oai-ausf:v1.4.0
 echo -e "${RED} Downloaded oai-ausf:v1.4.0 ${NC}"
-docker pull oaisoftwarealliance/oai-upf-vpp:v1.4.0
+sudo docker pull oaisoftwarealliance/oai-upf-vpp:v1.4.0
 echo -e "${RED} Downloaded oai-upf-vpp:v1.4.0 ${NC}"
-docker pull oaisoftwarealliance/oai-nssf:v1.4.0
+sudo docker pull oaisoftwarealliance/oai-nssf:v1.4.0
 echo -e "${RED} Downloaded oai-nssf:v1.4.0 ${NC}"
 # Utility image to generate traffic
-docker pull oaisoftwarealliance/trf-gen-cn5g:latest
+sudo docker pull oaisoftwarealliance/trf-gen-cn5g:latest
 echo -e "${RED} Downloaded trf-gen-cn5g:latest ${NC}"
 echo -e "Pull Completed"
